de159e73f091e14d28badf8c9bd3e51791e8c8ca
Harbor <mihaibo@tianyancha.com>
2026-07-08 16:03:06 +0800

    Add SubMind v1.0.0 cask

---
+++ Casks/submind.rb
@@ -0,0 +1,23 @@
+cask "submind" do
+  version "1.0.0"
+  sha256 "ef9adcf0e9d42f465fc69d21cfa97d8f63e3862f8fa03ad6456f182d976436fb"
+
+  url "https://github.com/AspirinMrmi/SubMind/releases/download/v#{version}/SubMind-#{version}.zip"
+  name "SubMind"
+  desc "macOS subscription manager - 付费订阅管理、到期提醒、统计仪表盘"
+  homepage "https://github.com/AspirinMrmi/SubMind"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :sonoma"
+
+  app "SubMind.app"
+
+  zap trash: [
+    "~/Library/Documents/SubMind",
+    "~/Library/Application Support/SubMind"
+  ]
+end
